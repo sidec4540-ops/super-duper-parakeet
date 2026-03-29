9fc7deb70c0d52a1b6aa84bc83cca3658f8085eb
sidec4540-ops <sidec4540@gmail.com>
2026-03-29 13:54:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,13 @@
 import time
-from pip._internal.cli.main import main
-
-# todo убрать когда-то
-
-try:
-    import lxml
-except ModuleNotFoundError:
-    main(["install", "-U", "lxml>=5.3.0"])
-except:
-    pass
-try:
-    import bcrypt
-except ModuleNotFoundError:
-    main(["install", "-U", "bcrypt>=4.2.0"])
-except:
-    pass
-try:
-    import socks
-except ModuleNotFoundError:
-    main(["install", "-U", "pysocks>=1.7.1"])
-except:
-    pass
-import Utils.cardinal_tools
-import Utils.config_loader as cfg_loader
-from first_setup import first_setup
-from colorama import Fore, Style
-from Utils.logger import LOGGER_CONFIG
-import logging.config
-import colorama
 import sys
 import os
+import logging.config
+import colorama
+from colorama import Fore, Style
+from Utils.logger import LOGGER_CONFIG
+import Utils.cardinal_tools
+import Utils.config_loader as cfg_loader
 from cardinal import Cardinal
-import Utils.exceptions as excs
 from locales.localizer import Localizer
 
 VERSION = "0.1.17.6"
@@ -48,81 +24,54 @@ for i in folders:
     if not os.path.exists(i):
         os.makedirs(i)
 
-files = ["configs/auto_delivery.cfg", "configs/auto_response.cfg"]
-for i in files:
-    if not os.path.exists(i):
-        with open(i, "w", encoding="utf-8") as f:
-            ...
-
 colorama.init()
-
 logging.config.dictConfig(LOGGER_CONFIG)
 logging.raiseExceptions = False
 logger = logging.getLogger("main")
-logger.debug("------------------------------------------------------------------")
 
 print(f"{Fore.RED}{Style.BRIGHT}v{VERSION}{Style.RESET_ALL}\n")
-print(f"{Fore.MAGENTA}{Style.BRIGHT}By {Fore.BLUE}{Style.BRIGHT}Woopertail, @sidor0912{Style.RESET_ALL}")
-print(f"{Fore.MAGENTA}{Style.BRIGHT} * GitHub: {Fore.BLUE}{Style.BRIGHT}github.com/sidor0912/FunPayCardinal{Style.RESET_ALL}")
-print(f"{Fore.MAGENTA}{Style.BRIGHT} * Telegram: {Fore.BLUE}{Style.BRIGHT}t.me/sidor0912")
-print(f"{Fore.MAGENTA}{Style.BRIGHT} * Новости о обновлениях: {Fore.BLUE}{Style.BRIGHT}t.me/fpc_updates")
-print(f"{Fore.MAGENTA}{Style.BRIGHT} * Плагины: {Fore.BLUE}{Style.BRIGHT}t.me/fpc_plugins")
-print(f"{Fore.MAGENTA}{Style.BRIGHT} * Донат: {Fore.BLUE}{Style.BRIGHT}t.me/sidor_donate")
-print(f"{Fore.MAGENTA}{Style.BRIGHT} * Telegram-чат: {Fore.BLUE}{Style.BRIGHT}t.me/funpay_cardinal")
+print(f"{Fore.MAGENTA}{Style.BRIGHT}By Woopertail, @sidor0912{Style.RESET_ALL}")
 
-# Пропускаем first_setup полностью
-if not os.path.exists("configs/_main.cfg"):
-    print("Файл configs/_main.cfg не найден, создаю пустой конфиг")
+# СОЗДАЕМ КОНФИГ ПРЯМО В КОДЕ
+MAIN_CFG = {
+    "FunPay": {
+        "golden_key": "dkpz660rypgawlmceydtnn2702tsbgjp",
+        "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
+        "autoRaise": "0",
+        "autoResponse": "0",
+        "autoDelivery": "0",
+        "multiDelivery": "0",
+        "autoRestore": "0",
+        "autoDisable": "0",
+        "oldMsgGetMode": "0",
+        "locale": "ru"
+    },
+    "Telegram": {
+        "enabled": "1",
+        "token": "8777560443:AAEwopxAHU6EtrZtJ5PXlVfYlp1wem9OV5c",
+        "user_id": "571001160",
+        "secretKeyHash": "MyPassword123",
+        "blockLogin": "0",
+        "proxy": ""
+    },
+    "Other": {
+        "watermark": "🐦",
+        "requestsDelay": "4",
+        "language": "ru"
+    }
+}
 
-if sys.platform == "linux" and os.getenv('FPC_IS_RUNNIG_AS_SERVICE', '0') == '1':
-    import getpass
-    pid = str(os.getpid())
-    pidFile = open(f"/run/FunPayCardinal/{getpass.getuser()}/FunPayCardinal.pid", "w")
-    pidFile.write(pid)
-    pidFile.close()
-    logger.info(f"PID файл создан, PID процесса: {pid}")
-
-# Загрузка конфига с обработкой ошибок
-try:
-    logger.info("Загружаю конфиг _main.cfg...")
-    MAIN_CFG = cfg_loader.load_main_config("configs/_main.cfg")
-except Exception as e:
-    logger.error(f"Ошибка загрузки конфига: {e}")
-    logger.info("Создаю конфиг по умолчанию...")
-    MAIN_CFG = cfg_loader.create_config_obj({})
-    MAIN_CFG["Other"] = {"language": "ru"}
-    MAIN_CFG["Telegram"] = {"enabled": "1"}
-    MAIN_CFG["FunPay"] = {}
-
-try:
-    localizer = Localizer(MAIN_CFG.get("Other", {}).get("language", "ru"))
-    _ = localizer.translate
-except:
-    localizer = Localizer("ru")
-    _ = localizer.translate
+localizer = Localizer("ru")
+_ = localizer.translate
 
 try:
-    logger.info("Загружаю конфиг auto_response.cfg...")
-    AR_CFG = cfg_loader.load_auto_response_config("configs/auto_response.cfg")
-    RAW_AR_CFG = cfg_loader.load_raw_auto_response_config("configs/auto_response.cfg")
-except:
-    AR_CFG = {}
-    RAW_AR_CFG = {}
-
-try:
-    logger.info("Загружаю конфиг auto_delivery.cfg...")
-    AD_CFG = cfg_loader.load_auto_delivery_config("configs/auto_delivery.cfg")
-except:
-    AD_CFG = {}
-
-try:
-    Cardinal(MAIN_CFG, AD_CFG, AR_CFG, RAW_AR_CFG, VERSION).init().run()
+    logger.info("Запуск бота...")
+    Cardinal(MAIN_CFG, {}, {}, {}, VERSION).init().run()
 except KeyboardInterrupt:
     logger.info("Завершаю программу...")
     sys.exit()
 except Exception as e:
-    logger.critical(f"При работе Кардинала произошла ошибка: {e}")
+    logger.critical(f"Ошибка: {e}")
     logger.warning("TRACEBACK", exc_info=True)
-    logger.critical("Завершаю программу...")
     time.sleep(5)
     sys.exit()
